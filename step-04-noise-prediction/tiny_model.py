d71193d3da78156515ba7977ae155227b7eba942
Adarsh <aadarshjha07137@gmail.com>
2026-06-08 13:35:29 +0530

    applying loss function and optim

--- step-04-noise-prediction/tiny_model.py
+++ step-04-noise-prediction/tiny_model.py
@@ -1,18 +1,26 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
 model = nn.Linear(4, 2)
 
 input_data = torch.tensor([[1.0, 2.0, 3.0, 4.0]])
 
-target = torch.tensor([[0.5, 1.0]])
-
-output = model(input_data)
+target = torch.tensor([[0.2, 0.1]])
 
 loss_function = nn.MSELoss()
 
-loss = loss_function(output, target)
+optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-print(output)
-
-print(loss)
+for step in range(10):
+  output = model(input_data)
+  
+  loss = loss_function(output, target)
+  
+  optimizer.zero_grad()
+  
+  loss.backward()
+  
+  optimizer.step()
+  
+  print(f"Step {step}: Loss = {loss.item()}")
